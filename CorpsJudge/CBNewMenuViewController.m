899ed2173c4aa5d3f78b34e69f03bd2a5815e608
JustinDMoore <Justin@Justins-Office-2.local>
2015-06-15 20:35:55 -0700

    Fixed DCI logo (should be cadets) for push.

--- CorpsJudge/CBNewMenuViewController.m
+++ CorpsJudge/CBNewMenuViewController.m
@@ -1144,22 +1144,24 @@ bool isScrolling = NO;
 
 - (IBAction)btnNearMe_tapped:(id)sender {
 
+    [self askForLocationPermission];
+    
     if([CLLocationManager locationServicesEnabled]){
         
         NSLog(@"Location Services Enabled");
         
-        switch ([CLLocationManager authorizationStatus]) {
-            case kCLAuthorizationStatusDenied: [self tellUserToEnableLocation];
-                break;
-            case kCLAuthorizationStatusNotDetermined: [self askForLocationPermission];
-                break;
-            case kCLAuthorizationStatusAuthorizedAlways: [self performSegueWithIdentifier:@"find" sender:self];
-                break;
-            case kCLAuthorizationStatusAuthorizedWhenInUse: [self performSegueWithIdentifier:@"find" sender:self];
-                break;
-            case kCLAuthorizationStatusRestricted: [self tellUserToEnableLocation];
-                break;
-        }
+//        switch ([CLLocationManager authorizationStatus]) {
+//            case kCLAuthorizationStatusDenied: [self tellUserToEnableLocation];
+//                break;
+//            case kCLAuthorizationStatusNotDetermined: [self askForLocationPermission];
+//                break;
+//            case kCLAuthorizationStatusAuthorizedAlways: [self performSegueWithIdentifier:@"find" sender:self];
+//                break;
+//            case kCLAuthorizationStatusAuthorizedWhenInUse: [self performSegueWithIdentifier:@"find" sender:self];
+//                break;
+//            case kCLAuthorizationStatusRestricted: [self tellUserToEnableLocation];
+//                break;
+//        }
     
     } else {
         NSLog(@"iOS location services disabled.");
