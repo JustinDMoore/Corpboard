b38b3242e5e7e4649eaa0776af756787aed618e7
JustinDMoore <Justin@Justins-Office-2.local>
2015-08-16 09:29:55 -0700

    Daily schedule

---
+++ CorpsJudge/DailyScheduleModel.swift
@@ -0,0 +1,55 @@
+//
+//  DailyScheduleModel.swift
+//  Corpboard
+//
+//  Created by Justin Moore on 8/14/15.
+//  Copyright (c) 2015 Justin Moore. All rights reserved.
+//
+
+import Foundation
+
+let toppings = [
+    
+    // MORNING
+    
+                "Up":"waking up",
+                "The day":"starting their day",
+                "S & R":"doing physical conditioning",
+    
+    // FOOD
+    
+                "Breakfast":"having breakfast",
+                "Lunch":"having lunch",
+                "Dinner":"having dinner",
+                "Snack":"having a snack",
+    
+    // REHEARSAL
+    
+                "Visual Rehearsal":"having visual rehearsal",
+                "Sectionals":"having sectional rehearsal",
+                "Setup":"setting up for ensemble rehearsal",
+                "Ensemble":"having ensemble rehearsal",
+    
+    
+    "Dinner and Pack":"having dinner and packing equipment",
+    
+    // SHOW STUFF
+                "Depart to Show":"departing to the show",
+                "Arrive at Show":"arriving for the show",
+                "Warmup":"warming up for the show",
+                "Gate":"waiting at the gate to perform",
+                "Perform":"performing The Power of 10",
+    
+    // MISC STUFF
+                "BIS":"sitting on the buses waiting to depart",
+                "Asleep":"sleeping",
+                "Lights Out":"sleeping",
+                "With Hopkins":"meeting with Hopkins",
+                "EPL":"having dinner and preparing to depart",
+                "Depart":"traveling",
+    
+
+
+
+
+]
