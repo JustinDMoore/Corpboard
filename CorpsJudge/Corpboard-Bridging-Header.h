90cb0dedb91a237314ed99a3080aed33f8ccd77e
Justin Moore <JustinDMoore@icloud.com>
2016-05-13 19:36:06 -0700

    Organization

--- CorpsJudge/Corpboard-Bridging-Header.h
+++ CorpsJudge/Corpboard-Bridging-Header.h
@@ -6,37 +6,46 @@
 //  Copyright (c) 2015 Justin Moore. All rights reserved.
 //
 
-#import <Parse/Parse.h>
-#import <ParseUI/ParseUI.h>
-#import <Stripe/Stripe.h>
-#import <Stripe/Stripe+ApplePay.h>
-#import <Parse/PFObject+Subclass.h>
+
 #import "ClipView.h"
 #import "CBStoreCategoryCell.h"
 #import "Configuration.h"
-#import <MWFeedParser/MWFeedParser.h>
-#import "CadetsAni.h"
+#import "CadetsAnimation.h"
 #import "AppConstant.h"
-#import <MWFeedParser/NSDate+InternetDateTime.h>
 #import "CBNewsCell.h"
 #import "CBWebViewController.h"
 #import "CBShowDetailsViewController.h"
 #import "CBUserProfileViewController.h"
 #import "CBLocationServices.h"
 #import "CBNewsCell.h"
-#import <MWFeedParser/MWFeedItem.h>
+#import "CBPush.h"
 
 // pods
-#import <KVNProgress/KVNProgress.h>
-#import <JSBadgeView/JSBadgeView.h>
-#import <PulsingHalo/PulsingHaloLayer.h>
+
+#import <Parse/Parse.h>
+#import <ParseUI/ParseUI.h>
 #import <Parse/PFObject+Subclass.h>
-#import <Stripe/Stripe.h>
-#import <AFNetworking/AFNetworking.h>
 #import <ParseFacebookUtilsV4/PFFacebookUtils.h>
+
+#import <Stripe/Stripe.h>
+#import <Stripe/Stripe+ApplePay.h>
+
+#import <KVNProgress/KVNProgress.h>
+
+#import <MWFeedParser/MWFeedParser.h>
+#import <MWFeedParser/MWFeedItem.h>
+#import <MWFeedParser/NSDate+InternetDateTime.h>
+
+#import <JSBadgeView/JSBadgeView.h>
+
+#import <PulsingHalo/PulsingHaloLayer.h>
+
+#import <AFNetworking/AFNetworking.h>
+
 #import <IQKeyboardManager/IQKeyboardManager.h>
+
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
-#import "CBPush.h"
+
 
 #ifndef Corpboard_Corpboard_Bridging_Header_h
 #define Corpboard_Corpboard_Bridging_Header_h
