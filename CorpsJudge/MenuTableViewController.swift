52f4f5e46b4dd642d0c2011b1ae13d2606f81a29
Justin Moore <JustinDMoore@icloud.com>
2016-06-09 18:21:00 -0700

    Tapping on a recent or upcoming show from the menu now opens it again.

--- CorpsJudge/MenuTableViewController.swift
+++ CorpsJudge/MenuTableViewController.swift
@@ -536,51 +536,12 @@ class MenuTableViewController: UITableViewController, UICollectionViewDelegate, 
         }
         return UITableViewCell()
     }
-    
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if tableView == tableLastShows || tableView == tableNextShows {
+            self.openShowAtIndex(indexPath, tableView: tableView)
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     
     //MARK:-
@@ -1265,13 +1226,13 @@ class MenuTableViewController: UITableViewController, UICollectionViewDelegate, 
     }
     
     func openShowAtIndex(indexPath: NSIndexPath, tableView: UITableView) {
-        if tableView === self.tableLastShows {
-            self.showToOpen = self.arrayOfShowsForTable1[indexPath.row]
+        if tableView === tableLastShows {
+            self.showToOpen = arrayOfShowsForTable1[indexPath.row]
             if self.showToOpen != nil {
                 self.performSegueWithIdentifier("openShow", sender: self)
             }
-        } else if tableView === self.tableNextShows {
-            self.showToOpen = self.arrayOfShowsForTable2[indexPath.row]
+        } else if tableView === tableNextShows {
+            self.showToOpen = arrayOfShowsForTable2[indexPath.row]
             if self.showToOpen != nil {
                 self.performSegueWithIdentifier("openShow", sender: self)
             }
